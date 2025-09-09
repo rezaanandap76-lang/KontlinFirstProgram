13b80adf782f7bef0b28d71bdd0af48f52fb334b
rezaanandap76-lang <rezaanandap76@gmail.com>
2025-09-09 22:36:20 +0700

    Commit 1Komentar

---
+++ src/1Komentar.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello, Ini Percobaan Blok dan Komentar Pertama Saya di Kotlin!")
+//Contoh komentar satu baris
+    /*
+    Contoh komentar multi baris
+    */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
